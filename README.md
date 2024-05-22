0af39403a22aaa83c9b9c844400060eb70bf3f34
Cameron Gridley <cgridley@gmail.com>
2024-05-22 09:40:31 -0600

    Created generic base class for data generation. Updated Readme.

--- README.md
+++ README.md
@@ -8,11 +8,14 @@ local development. Pydantic is used in conjunction with Faker to provide the sch
 for the fake data.
 
 ## Project Setup
-1. Rename the source folder, via refactor, to match the repo name but with an underscore.
-2. Run `poetry install --sync` (sync is optional)
-3. Run `poetry shell` if the virtual environment wasn't automatically activated.
-4. Run `which python` to get the path of the python interpreter
-5. Link project to the python interpreter:
+1. Clone this repo to your local system.
+2. Install [poetry](https://python-poetry.org/) using the official [installation instructions](https://python-poetry.org/docs/#installation).
+3. Rename the source folder, via refactor, to match the repo name but with an underscore.
+4. Run `poetry install --sync` (sync is optional)
+   - This will create a virtual environment and install all the libraries specified in `pyproject.toml` there.
+5. Run `poetry shell` if the virtual environment wasn't automatically activated.
+6. Run `which python` to get the path of the python interpreter
+7. Link the project to the python interpreter in the virual environment:
    - IntelliJ: 
      - File --> 
        - Project Structure --> 
@@ -20,18 +23,30 @@ for the fake data.
            - Add Pyton SDK --> 
              - Existing Environment --> 
                - Click `...` and enter path from step 4
-6. Click OK and you should be able to run `main.py`
+8. Click OK and you should be able to run `main.py`
 
 ## Running the Project
 Run main.py for sample job that generates fake data and uses the transformations found in the transformations module.
+
+### Sprak App Project Structure
+- TODO
+
+### Local Development
+- TODO
+
+### Spark Unit Testing
+- TODO
 
 ## Fake Data Generation
 The app processes data from a fictional content provider. The tables are `user`, `content` and `activity`.
 You can create the data dynamically as dataframes or you can save the data to parquet or csv.
 
-The `StreamingServiceDataGenerator` class handles all the fake data generation.
-- Instantiate a class, specifying the number of users, content and activity you want created.
-- Generate the data bby calling, `StreamingServiceDataGenerator.generate_streaming_service_data()`
+`DataGenerator` is the base class for data generation. It handles common functions like converting
+models and saving dataframes to file.
+The child class, `StreamingServiceDataGenerator`, handles all the fake data generation for the streaming service.
+To use it:
+- Instantiate a `StreamingServiceDataGenerator` object, specifying the number of users, content and activity you want created.
+- Generate the data by calling, `StreamingServiceDataGenerator.generate_streaming_service_data()`
   - This returns 3 dataframes, users_df, content_df and activity_df
 - Save the data to file by calling`StreamingServiceDataGenerator.save_streaming_data()` and specifying the base path and the file format.
   - By default, parquet files are creates, but you can pass "csv" to the `format` arg if you prefer csv.
--- pyspark_template/utils/generate_data.py
+++ pyspark_template/utils/generate_data.py
@@ -2,45 +2,75 @@ from pyspark_template.models.user import User
 from pyspark_template.models.content import Content
 from pyspark_template.models.activity import Activity
 from faker import Faker
-import uuid
 from pyspark.sql import SparkSession, DataFrame
 from pydantic import BaseModel
+from pyspark.sql.types import StructType
 
 """
-This group of functions is for creating fake data for use in this template repo. 
+This group of classes is for creating fake data for use in this template repo. 
 It uses the Faker library to generate data and Pydantic models to define the schema and validate generated data.
 """
 
 
-def generate_unique_id(start=1):
-    """
-    Generate a unique id starting at the start parameter.
-    :param start:int: the number to start with
-    :return int: an id
-    """
-    while True:
-        yield start
-        start += 1
+class DataGenerator():
+    def __init__(self, spark_session: SparkSession):
+        """
+        Initialize data generator.
+        :param spark_session: SparkSession
+        """
+        self.spark = spark_session
 
-def save_data(df: DataFrame, file_format: str, path: str, mode_choice="errorifexists"
-              ) -> None:
-    """
-    Save a DataFrame file of the specified format.
-    :param df: DataFrame to save
-    :param file_format: File format to save as
-    :param path: Path to save to
-    :param mode_choice: Spark write mode, defaults to "errorifexists". Can also be "overwrite" or "append".
-    :return:
-    """
-    if file_format == "parquet":
-        df.write.mode(mode_choice).parquet(path)
-    elif file_format == "csv":
-        df.write.mode(mode_choice).csv(path)
-    else:
-        raise Exception("Invalid file format.")
+    def _create_empty_dataframe(self) -> DataFrame:
+        """
+        Create and empty dataframe. Can be used for instantiating class dataframe variables.
+        :return: DataFrame
+        """
+        schema = StructType([])
+
+        return self.spark.createDataFrame(self.spark.sparkContext.emptyRDD(), schema)
+
+    def convert_model_list_to_dataframe(self, model_list: list[BaseModel]
+                                        ) -> DataFrame:
+        """
+        Convert a list of Pydantic models to a Spark DataFrame.
+        :param model_list: List of Pydantic models
+        :return: DataFrame
+        """
+        json_data = [d.json() for d in model_list]
+        rdd = self.spark.sparkContext.parallelize(json_data)
+        df = self.spark.read.json(rdd)
+
+        return df
+
+    def generate_unique_id(self, start=1):
+        """
+        Generate a unique id starting at the start parameter.
+        :param start:int: the number to start with
+        :return int: an id
+        """
+        while True:
+            yield start
+            start += 1
+
+    def save_data(self, df: DataFrame, file_format: str, path: str, mode_choice="errorifexists"
+                  ) -> None:
+        """
+        Save a DataFrame file of the specified format.
+        :param df: DataFrame to save
+        :param file_format: File format to save as
+        :param path: Path to save to
+        :param mode_choice: Spark write mode, defaults to "errorifexists". Can also be "overwrite" or "append".
+        :return:
+        """
+        if file_format == "parquet":
+            df.write.mode(mode_choice).parquet(path)
+        elif file_format == "csv":
+            df.write.mode(mode_choice).csv(path)
+        else:
+            raise Exception("Invalid file format.")
 
 
-class StreamingDataGenerator():
+class StreamingDataGenerator(DataGenerator):
     def __init__(self, spark_session: SparkSession, n_users: int, n_content: int, n_activity: int):
         """
         Initialize data generator.
@@ -49,13 +79,14 @@ class StreamingDataGenerator():
         :param n_content: Length of the content list
         :param n_activity: Number of activity rows to create
         """
+        super().__init__(spark_session)
         self.spark = spark_session
         self.n_users = n_users
         self.n_content = n_content
         self.n_activity = n_activity
-        self.users_df: DataFrame = None
-        self.content_df: DataFrame = None
-        self.activity_df: DataFrame = None
+        self.users_df: DataFrame = self._create_empty_dataframe()
+        self.content_df: DataFrame = self._create_empty_dataframe()
+        self.activity_df: DataFrame = self._create_empty_dataframe()
 
     def generate_fake_user_data(self) -> list[User]:
         """
@@ -63,7 +94,7 @@ class StreamingDataGenerator():
         :return: List of User objects
         """
         fake = Faker()
-        id_generator = generate_unique_id()
+        id_generator = self.generate_unique_id()
         users = []
         for _ in range(self.n_users):
             user = User(
@@ -82,8 +113,9 @@ class StreamingDataGenerator():
                 ),
             )
             users.append(user)
-        return users
 
+        print(f'Generated {len(users)} users')
+        return users
 
     def generate_fake_content_data(self) -> list[Content]:
         """
@@ -91,7 +123,7 @@ class StreamingDataGenerator():
         :return: List of Content objects
         """
         fake = Faker()
-        id_generator = generate_unique_id()
+        id_generator = self.generate_unique_id()
         ratings = ["G", "PG", "PG-13", "R"]
         content_list = []
 
@@ -104,8 +136,8 @@ class StreamingDataGenerator():
             )
             content_list.append(content)
 
+        print(f'Generated {len(content_list)} content')
         return content_list
-
 
     def generate_fake_activity(self) -> list[Activity]:
         """
@@ -114,7 +146,7 @@ class StreamingDataGenerator():
         :return: List of Activity objects
         """
         fake = Faker()
-        id_generator = generate_unique_id()
+        id_generator = self.generate_unique_id()
         activity_list = []
 
         for _ in range(self.n_activity):
@@ -128,22 +160,8 @@ class StreamingDataGenerator():
 
             activity_list.append(activity)
 
+        print(f'Generated {len(activity_list)} activities')
         return activity_list
-
-
-    def convert_model_list_to_dataframe(self, model_list: list[BaseModel]
-    ) -> DataFrame:
-        """
-        Convert a list of Pydantic models to a Spark DataFrame.
-        :param model_list: List of Pydantic models
-        :return: DataFrame
-        """
-        json_data = [d.json() for d in model_list]
-        rdd = self.spark.sparkContext.parallelize(json_data)
-        df = self.spark.read.json(rdd)
-
-        return df
-
 
     def generate_streaming_service_data(self) -> tuple[DataFrame, DataFrame, DataFrame]:
         """
@@ -155,14 +173,12 @@ class StreamingDataGenerator():
         fake_activity = self.generate_fake_activity()
 
         self.users_df = self.convert_model_list_to_dataframe(fake_users)
-
         self.content_df = self.convert_model_list_to_dataframe(fake_content)
-
         self.activity_df = self.convert_model_list_to_dataframe(fake_activity)
 
         return self.users_df, self.content_df, self.activity_df
 
-    def save_streaming_data(self, base_save_path: str, mode_choice = "overwrite") -> None:
+    def save_streaming_data(self, base_save_path: str, mode_choice="overwrite") -> None:
         """
         Save streaming data to parquet files.
         :param base_save_path: Base path to save to
@@ -170,36 +186,39 @@ class StreamingDataGenerator():
         :return: None
         """
 
-        save_data(
+        self.save_data(
             df=self.users_df,
             file_format="parquet",
             path=f"{base_save_path}/users",
             mode_choice=mode_choice,
         )
+        print(f'Saved {self.users_df.count()} users data to file.')
 
-        save_data(
+        self.save_data(
             df=self.content_df,
             file_format="parquet",
             path=f"{base_save_path}/content",
             mode_choice=mode_choice,
         )
+        print(f'Saved {self.content_df.count()} content data to file.')
 
-        save_data(
+        self.save_data(
             df=self.activity_df,
             file_format="parquet",
             path=f"{base_save_path}/activity",
             mode_choice=mode_choice,
         )
+        print(f'Saved {self.activity_df.count()} activity data to file.')
+
 
 if __name__ == "__main__":
     from pyspark_template.utils.spark_session import get_spark_session
 
     spark = get_spark_session()
-    n_users = 100000
-    n_content = 20000
-    n_activity = 300000
+    n_users = 1000
+    n_content = 200
+    n_activity = 3000
 
     streaming_data_generator = StreamingDataGenerator(spark, n_users, n_content, n_activity)
     streaming_data_generator.generate_streaming_service_data()
-    streaming_data_generator.save_streaming_data(base_save_path="../data/streaming", mode_choice="overwrite")
-
+    streaming_data_generator.save_streaming_data(base_save_path="../data/streaming", mode_choice="append")
